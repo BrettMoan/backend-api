c5a4e95818c16f78691794f7d29530f4211fef85
Brett Moan <6159590+BrettMoan@users.noreply.github.com>
2026-01-11 22:45:31 -0700

    fix build

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 
+
 data "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,5 @@
 # Define outputs here
+output "api_gateway_url" {
+  description = "The invoke URL of the API Gateway HTTP API."
+  value       = aws_apigatewayv2_api.fastapi.api_endpoint
+}
